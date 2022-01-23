2b553c5c53783cc063cbddfb373f87b2d9dc779f
Avneet <avneetmaankiya@gmail.com>
2022-01-24 03:01:44 +0530

    TwoWayDataBinding

---
+++ TwoWayDataBinding/app/src/main/java/com/example/twowaydatabinding/MainActivityViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.twowaydatabinding
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+
+    val userName = MutableLiveData<String>()
+
+    init {
+        userName.value = "Avneet"
+    }
+
+}
